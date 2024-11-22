693f3c9f19a1ca5a4a9cd3529e90b4cd54ec7327
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-11-22 12:09:05 +0500

    create slider-employee-reviews

--- js/sliderTogetherSwiper.js
+++ js/sliderTogetherSwiper.js
@@ -1,12 +1,21 @@
 var swiper = new Swiper(".sliderTogetherSwiper", {
   cssMode: true,
+  spaceBetween: 10,
+  centeredSlides: true,
+  speed: 3500,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
   mousewheel: true,
-  keyboard: true,  
+  keyboard: true,
 });
+
