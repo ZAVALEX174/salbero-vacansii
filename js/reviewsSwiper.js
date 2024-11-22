0df73a281200cc7f615906668bac4c7033e9b585
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-11-22 14:10:31 +0500

    create reviewsSwiper

---
+++ js/reviewsSwiper.js
@@ -0,0 +1,15 @@
+var employeeReviewsSwiper = new Swiper(".reviewsSwiper", {
+  cssMode: true,
+  spaceBetween: 10,
+  centeredSlides: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+});
