f8bea6ec8d9a78ad6991a7d6cbf4b457d5201afd
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-11-26 13:13:27 +0500

    awardsSwiper mobile(not pixel)

--- js/awardsSwiper.js
+++ js/awardsSwiper.js
@@ -1,6 +1,15 @@
 var awardsSwiper = new Swiper(".awardsSwiper", {
   slidesPerView: 4.5,
   spaceBetween: 30,
+  loop: true,
   // centeredSlides: true,
   // autoHeight: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    }
+    , 621: {
+      slidesPerView: 4.5,
+    }
+  }
 });
