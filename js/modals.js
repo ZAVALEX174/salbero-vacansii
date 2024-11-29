65ff97351a2d1301fba80352f825486be51907b4
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-11-29 20:22:54 +0500

    modal-form modal-form-zayavka <script src="js/modals.js" defer></script> и добавились стили в CSS

---
+++ js/modals.js
@@ -0,0 +1,38 @@
+
+const modal = document.querySelector('.modal-form');
+const openModalBtns = document.querySelectorAll('.openModal');
+const closesModal = document.querySelector('.modal-form__close-btn');
+
+const modalTwo  = document.querySelector('.modal-form-zayavka');
+const modalTwoOpenBtn = document.querySelector('.hero__entry-form-btn');
+
+modalTwoOpenBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    modalTwo.classList.add('modal-form__opened');
+    document.body.classList.add('scroll-lock');
+})
+
+
+openModalBtns.forEach(el => {
+    el.addEventListener('click', (e) => {
+        e.preventDefault();
+        modal.classList.add('modal-form__opened');
+        document.body.classList.add('scroll-lock');
+    })
+})
+
+closesModal.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    modal.classList.remove('modal-form__opened');
+    modalTwo.classList.remove('modal-form__opened');
+    document.body.classList.remove('scroll-lock');
+})
+
+document.addEventListener('keydown', event => {
+    if (event.key === "Escape" || event.keyCode === 27) {
+        modalTwo.classList.remove('modal-form__opened');
+        modal.classList.remove('modal-form__opened');
+        document.body.classList.remove('scroll-lock');
+    }
+});
